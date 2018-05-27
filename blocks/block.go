56d6b5038fd62d7a5e901456da50416431f922c4
Ivan Santos <301291+pragmaticivan@users.noreply.github.com>
2018-05-27 09:00:27 -0700

    Extracts block to another package

---
+++ blocks/block.go
@@ -0,0 +1,34 @@
+package blocks
+
+import (
+	"fmt"
+	"time"
+)
+
+// Block - working block
+type Block struct {
+	ID        string   `json:"id,omitempty"`
+	StartTime JSONTime `json:"start_time,omitempty"`
+	EndTime   JSONTime `json:"end_time,omitempty"`
+	Timezone  string   `json:"timezone,omitempty"`
+	UserID    string   `json:"user_id,omitempty"`
+}
+
+// Marshaler -
+type Marshaler interface {
+	MarshalJSON() ([]byte, error)
+}
+
+// JSONTime -
+type JSONTime time.Time
+
+// MarshalJSON -
+func (t JSONTime) MarshalJSON() ([]byte, error) {
+	stamp := fmt.Sprintf("\"%s\"", time.Time(t).Format("15:04"))
+	return []byte(stamp), nil
+}
+
+// HealthCheck - status
+type HealthCheck struct {
+	Status string `json:"status"`
+}
--- main.go
+++ main.go
@@ -9,43 +9,15 @@ import (
 
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
+	block "github.com/remoteview/service-blocks/blocks"
 )
 
-// Block - working block
-type Block struct {
-	ID        string   `json:"id,omitempty"`
-	StartTime JSONTime `json:"start_time,omitempty"`
-	EndTime   JSONTime `json:"end_time,omitempty"`
-	Timezone  string   `json:"timezone,omitempty"`
-	UserID    string   `json:"user_id,omitempty"`
-}
-
-// Marshaler -
-type Marshaler interface {
-	MarshalJSON() ([]byte, error)
-}
-
-// JSONTime -
-type JSONTime time.Time
-
-// MarshalJSON -
-func (t JSONTime) MarshalJSON() ([]byte, error) {
-	//do your serializing here
-	stamp := fmt.Sprintf("\"%s\"", time.Time(t).Format("15:04"))
-	return []byte(stamp), nil
-}
-
-// HealthCheck - status
-type HealthCheck struct {
-	Status string `json:"status"`
-}
-
-var blocks []Block
+var blocks []block.Block
 
 func main() {
-	blocks = append(blocks, Block{ID: "1", StartTime: JSONTime(time.Date(0, 0, 0, 9, 0, 0, 0, time.UTC)), EndTime: JSONTime(time.Date(0, 0, 0, 17, 0, 0, 0, time.UTC)), Timezone: "UTC", UserID: "1"})
-	blocks = append(blocks, Block{ID: "2", StartTime: JSONTime(time.Date(0, 0, 0, 9, 0, 0, 0, time.UTC)), EndTime: JSONTime(time.Date(0, 0, 0, 17, 0, 0, 0, time.UTC)), Timezone: "UTC", UserID: "2"})
-	blocks = append(blocks, Block{ID: "3", StartTime: JSONTime(time.Date(0, 0, 0, 9, 0, 0, 0, time.UTC)), EndTime: JSONTime(time.Date(0, 0, 0, 17, 0, 0, 0, time.UTC)), Timezone: "UTC", UserID: "3"})
+	blocks = append(blocks, block.Block{ID: "1", StartTime: block.JSONTime(time.Date(0, 0, 0, 9, 0, 0, 0, time.UTC)), EndTime: block.JSONTime(time.Date(0, 0, 0, 17, 0, 0, 0, time.UTC)), Timezone: "UTC", UserID: "1"})
+	blocks = append(blocks, block.Block{ID: "2", StartTime: block.JSONTime(time.Date(0, 0, 0, 9, 0, 0, 0, time.UTC)), EndTime: block.JSONTime(time.Date(0, 0, 0, 17, 0, 0, 0, time.UTC)), Timezone: "UTC", UserID: "2"})
+	blocks = append(blocks, block.Block{ID: "3", StartTime: block.JSONTime(time.Date(0, 0, 0, 9, 0, 0, 0, time.UTC)), EndTime: block.JSONTime(time.Date(0, 0, 0, 17, 0, 0, 0, time.UTC)), Timezone: "UTC", UserID: "3"})
 
 	router := mux.NewRouter()
 	router.HandleFunc("/_health", healthCheckHandler).Methods("GET")
@@ -57,16 +29,20 @@ func main() {
 	methodsOk := handlers.AllowedMethods([]string{"GET", "POST", "OPTIONS"})
 
 	fmt.Println("Running server!")
-	log.Fatal(http.ListenAndServe(":3000", handlers.CORS(originsOk, headersOk, methodsOk)(router)))
+	log.Fatal(http.ListenAndServe(":3001", handlers.CORS(originsOk, headersOk, methodsOk)(router)))
 }
 
 func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode(HealthCheck{Status: "Ok"})
+	err := json.NewEncoder(w).Encode(block.HealthCheck{Status: "Ok"})
+	if err != nil {
+
+	}
 }
 
 func listBlocksHandler(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(blocks)
 }
+
 func listBlockHandler(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	for _, item := range blocks {
@@ -75,5 +51,5 @@ func listBlockHandler(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 	}
-	json.NewEncoder(w).Encode(&Block{})
+	json.NewEncoder(w).Encode(&block.Block{})
 }
